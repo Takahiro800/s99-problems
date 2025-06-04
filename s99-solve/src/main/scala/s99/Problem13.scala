33c6e5f905b1d61c820f782478ec3b3d2b2a3c6f
taemath <hirotaka081630@gmail.com>
2025-06-05 04:21:32 +0900

    refactor problem13

--- s99-solve/src/main/scala/s99/Problem13.scala
+++ s99-solve/src/main/scala/s99/Problem13.scala
@@ -1,12 +1,10 @@
 package s99
 
 object Problem13 {
-  def encodeDirect[A](list: List[A]): List[(Int, A)] = list match {
-    case Nil => Nil
-    case _ =>
-      list.foldRight(Nil) { (char, acc) =>
-        if (acc.isEmpty || acc.head._2 != char) (1, char) :: acc
-        else (acc.head._1 + 1, acc.head._2) :: acc.tail
-      }
-  }
+  def encodeDirect[A](list: List[A]): List[(Int, A)] =
+    list.foldRight(Nil) {
+      case (ele, (count, char) :: tail) if ele == char =>
+        (count + 1, char) :: tail
+      case (ele, acc) => (1, ele) :: acc
+    }
 }
